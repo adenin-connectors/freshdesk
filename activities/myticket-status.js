6abdce10d8a18a5c584f21e2dfa75ce69aa24426
snt_danilo_markovic <dmarkovic@smartnet.rs>
2019-03-01 16:20:38 +0100

    initial commit.

---
+++ activities/myticket-status.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const cfActivity = require('@adenin/cf-activity');
+const api = require('./common/api');
+
+module.exports = async (activity) => {
+  try {
+    api.initialize(activity);
+
+    const response = await api('/tickets');
+
+    if (!cfActivity.isResponseOk(activity, response)) {
+      return;
+    }
+
+    let tickets = response.body;
+    let freshdeskDomain = api.getDomain();
+
+    let ticketStatus = {
+      title: 'Freshdesk Tickets',
+      url: `https://${freshdeskDomain}/a/tickets/filters/all_tickets`,
+      urlLabel: 'All Tickets',
+    };
+
+    let noOfTickets = tickets.length;
+
+    if (noOfTickets > 0) {
+      ticketStatus = {
+        ...ticketStatus,
+        description: `You have ${noOfTickets > 1 ? noOfTickets + " tickets" : noOfTickets + " ticket"}`,
+        color: 'blue',
+        value: noOfTickets,
+        actionable: true
+      }
+    } else {
+      ticketStatus = {
+        ...ticketStatus,
+        description: `You have no tickets.`,
+        actionable: false
+      }
+    }
+
+    activity.Response.Data = ticketStatus;
+
+  } catch (error) {
+    cfActivity.handleError(error, activity);
+  }
+};
