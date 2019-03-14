0d3db5a1573c83e04c97bdc400b55f2b2b4e42b7
snt_danilo_markovic <dmarkovic@smartnet.rs>
2019-03-14 14:31:32 +0100

    added new activity 'mytickets.js'.

---
+++ activities/mytickets.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const logger = require('@adenin/cf-logger');
+const cfActivity = require('@adenin/cf-activity');
+const api = require('./common/api');
+
+module.exports = async (activity) => {
+  try {
+    api.initialize(activity);
+
+    var pagination = cfActivity.pagination(activity);
+
+    const response = await api(`/tickets?page=${pagination.page}&per_page=${pagination.pageSize}`);
+
+    if (!cfActivity.isResponseOk(activity, response)) {
+      return;
+    }
+
+    activity.Response.Data = convertResponse(response);
+  } catch (error) {
+    cfActivity.handleError(error, activity);
+  }
+};
+/**maps response data to items */
+function convertResponse(response) {
+  let items = [];
+  let tickets = response.body;
+
+  let freshdeskDomain = api.getDomain();
+
+  for (let i = 0; i < tickets.length; i++) {
+    let raw = tickets[i];
+    let item = {
+      id: raw.id,
+      title: raw.subject,
+      description: raw.type,
+      link: `https://${freshdeskDomain}.freshdesk.com/a/tickets/${raw.id}`,
+      raw: raw
+    };
+    items.push(item);
+  }
+
+  return { items: items };
+}
