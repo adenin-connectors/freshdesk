dfbac57a3b74e114f5660aae32fa12fe791ab22a
snt_danilo_markovic <dmarkovic@smartnet.rs>
2019-06-20 09:33:43 +0200

    Code refactoring.

--- activities/tickets.js
+++ activities/tickets.js
@@ -18,23 +18,25 @@ module.exports = async (activity) => {
     let value = response.body.total;
 
     let freshdeskDomain = api.getDomain();
-    activity.Response.Data.items = api.convertResponse(response.body.results);
-    activity.Response.Data.title = T(activity, "All Tickets");
-    activity.Response.Data.link = `https://${freshdeskDomain}/a/tickets/filters/new_and_my_open`;
-    activity.Response.Data.linkLabel = T(activity, 'All Tickets');
-    activity.Response.Data.actionable = value > 0;
+    if (parseInt(pagination.page) == 1) {
+      activity.Response.Data.items = api.convertResponse(response.body.results);
+      activity.Response.Data.title = T(activity, "All Tickets");
+      activity.Response.Data.link = `https://${freshdeskDomain}/a/tickets/filters/new_and_my_open`;
+      activity.Response.Data.linkLabel = T(activity, 'All Tickets');
+      activity.Response.Data.actionable = value > 0;
 
-    if (value > 0) {
-      activity.Response.Data.value = value;
-      // items are alrady sorted by date descending (higest value first) in api request
-      // request wasn't changed it's just tested to see how it is sorted
-      // sort_by and sort_type can't be added to /search/tickets enpoint
-      activity.Response.Data.date = activity.Response.Data.items[0].date; 
-      activity.Response.Data.color = 'blue';
-      activity.Response.Data.description = value > 1 ? T(activity, "You have {0} tickets.", value) :
-        T(activity, "You have 1 ticket.");
-    } else {
-      activity.Response.Data.description = T(activity, `You have no tickets.`);
+      if (value > 0) {
+        activity.Response.Data.value = value;
+        // items are alrady sorted by date descending (higest value first) in api request
+        // request wasn't changed it's just tested to see how it is sorted
+        // sort_by and sort_type can't be added to /search/tickets enpoint
+        activity.Response.Data.date = activity.Response.Data.items[0].date;
+        activity.Response.Data.color = 'blue';
+        activity.Response.Data.description = value > 1 ? T(activity, "You have {0} tickets.", value) :
+          T(activity, "You have 1 ticket.");
+      } else {
+        activity.Response.Data.description = T(activity, `You have no tickets.`);
+      }
     }
   } catch (error) {
     $.handleError(activity, error);
