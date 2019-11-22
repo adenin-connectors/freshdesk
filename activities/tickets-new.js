da5f55bdde6617198cf5a6c3b43b691716d7a1a5
olliejm <olliejm@protonmail.com>
2019-11-22 10:44:16 +0000

    tickets-new works

--- activities/tickets-new.js
+++ activities/tickets-new.js
@@ -19,25 +19,35 @@ module.exports = async (activity) => {
 
     if ($.isErrorResponse(activity, response)) return;
 
-    const value = response.body.total;
     const freshdeskDomain = api.getDomain();
 
-    activity.Response.Data.items = api.convertResponse(response.body.results);
+    const items = api.convertResponse(response.body.results);
+
+    let count = 0;
+    let readDate = (new Date(new Date().setDate(new Date().getDate() - 30))).toISOString(); // default read date 30 days in the past
+
+    if (activity.Request.Query.readDate) readDate = activity.Request.Query.readDate;
+
+    for (let i = 0; i < items.length; i++) {
+      if (items[i].date > readDate) count++;
+    }
+
+    activity.Response.Data.items = items;
 
     if (parseInt(pagination.page) === 1) {
       activity.Response.Data.title = T(activity, 'New Tickets');
       activity.Response.Data.link = `https://${freshdeskDomain}/a/tickets/filters/new_and_my_open`;
       activity.Response.Data.linkLabel = T(activity, 'All Tickets');
-      activity.Response.Data.actionable = value > 0;
+      activity.Response.Data.actionable = count > 0;
 
-      if (value > 0) {
-        activity.Response.Data.value = value;
+      if (count > 0) {
+        activity.Response.Data.value = count;
         // items are alrady sorted by date descending (higest value first) in api request
         // request wasn't changed it's just tested to see how it is sorted
         // sort_by and sort_type can't be added to /search/tickets enpoint
         activity.Response.Data.date = activity.Response.Data.items[0].date;
         activity.Response.Data.color = 'blue';
-        activity.Response.Data.description = value > 1 ? T(activity, 'You have {0} new tickets.', value) : T(activity, 'You have 1 new ticket.');
+        activity.Response.Data.description = count > 1 ? T(activity, 'You have {0} new tickets.', count) : T(activity, 'You have 1 new ticket.');
       } else {
         activity.Response.Data.description = T(activity, 'You have no new tickets.');
       }
